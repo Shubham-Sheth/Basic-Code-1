38877fbbf7876bfc708315795427308b5a55e570
Shubham-Sheth <74092104+Shubham-Sheth@users.noreply.github.com>
2020-11-07 13:38:30 +0530

    Add files via upload

---
+++ v.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double Num1,Num2,Num3;
+    cout << "Enter two numbers: ";
+    cin >> Num1 >> Num2;
+    Num3 = Num1 * Num2;
+    cout << "Product = " <<Num3;
+    return 0;
+}
